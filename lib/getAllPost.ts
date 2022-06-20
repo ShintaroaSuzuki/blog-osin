f9c910772aa6ed8433dfe79d49c79b65c67beddd
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2022-06-21 02:02:30 +0900

    [modify] 一覧ページで記事を新しい順に並べ替え

--- lib/getAllPost.ts
+++ lib/getAllPost.ts
@@ -3,15 +3,19 @@ import { Post } from "@/types";
 
 export const getAllPost = async () => {
   const posts: Post[] = [];
-  fs.readdirSync("./pages/posts").forEach(async (path) => {
+  const paths = fs.readdirSync("./pages/posts");
+  let importProcessed = 0;
+  paths.forEach(async (path) => {
     let module = await import(`../pages/posts/${path}`);
     let pathname = path.replace(".mdx", "");
     posts.push({ ...module.meta, pathname });
-  });
-  posts.sort((a, b) => {
-    if (a.createdAt < b.createdAt) return 1;
-    if (a.createdAt > b.createdAt) return -1;
-    return 0;
+    importProcessed++;
+    if (importProcessed == paths.length) {
+      posts.sort((a, b) => {
+        if (a.createdAt > b.createdAt) return -1;
+        return 1;
+      });
+    }
   });
   return posts;
 };
