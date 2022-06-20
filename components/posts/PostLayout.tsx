9676672d33d1b77ef95f5e197993dbddc8badd4e
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2022-06-20 14:33:58 +0900

    [modify] ダークモードじゃなくても文字色を反転させる

--- components/posts/PostLayout.tsx
+++ components/posts/PostLayout.tsx
@@ -2,7 +2,7 @@ import type { ReactChildren } from "react";
 
 const PostLayout = ({ children }: { children: ReactChildren }) => {
   return (
-    <article className="my-8 mx-auto w-5/6 prose prose-sm dark:prose-invert prose-h1:text-transparent prose-h1:bg-clip-text prose-h1:bg-gradient-to-r prose-h1:from-teal-400 prose-h1:to-purple-500">
+    <article className="my-8 mx-auto w-5/6 prose prose-sm prose-invert prose-h1:text-transparent prose-h1:bg-clip-text prose-h1:bg-gradient-to-r prose-h1:from-teal-400 prose-h1:to-purple-500">
       {children}
     </article>
   );
