f3cce49eb99a72247044c0292abed407ff1d925a
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2022-07-07 23:35:53 +0900

    [add] ヘッダーのデザインを変更

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex w-full justify-center items-center h-12 fixed z-10 bg-[#0F172A]">
+    <header className="flex w-full justify-center items-center h-12 fixed z-10 bg-[#0F172A] bg-opacity-95 backdrop-blur-sm">
       <Link href="/">
         <img
-          className="mb-1"
+          className="mb-1 z-20"
           src="/grinning-cat-with-smiling-eyes.png"
           alt="home header image"
           width={28}
