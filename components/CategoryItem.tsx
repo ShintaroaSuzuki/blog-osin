e6fd8d53a04cececb1578cc498956bc5e2170d9e
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2022-07-19 04:07:40 +0900

    [modify] UI の修正

--- components/CategoryItem.tsx
+++ components/CategoryItem.tsx
@@ -14,8 +14,8 @@ const CategoryItem = ({
         key={key}
         className="flex w-3/4 justify-between bg-slate-800 rounded-lg p-5"
       >
-        <p>{category.categoryName}</p>
-        <p>{category.articleCount}</p>
+        <p className="font-bold">{category.categoryName}</p>
+        <p className="text-slate-500">{category.articleCount}</p>
       </div>
     </Link>
   );
