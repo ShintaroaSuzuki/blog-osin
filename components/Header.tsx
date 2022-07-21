4fbc754c944c9b3252a9045691ea0f0e0969a3e3
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2022-07-21 18:45:02 +0900

    [add] Header にハンバーガーメニューを追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,46 @@
 import Link from "next/link";
+import { useState, useCallback } from "react";
+import HeaderMenu from "@/components/HeaderMenu";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenu = useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
   return (
-    <header className="flex w-full justify-center items-center h-12 fixed z-10 bg-slate-900 bg-opacity-100 backdrop-blur-sm">
-      <Link href="/">
-        <img
-          className="mb-1 z-20"
-          src="/grinning-cat-with-smiling-eyes.png"
-          alt="home header image"
-          width={28}
-          height={28}
-        />
-      </Link>
+    <header className="w-screen fixed z-10">
+      {isOpen && <HeaderMenu handleMenu={handleMenu} />}
+      <div className="flex w-full justify-between items-center px-6 h-12 bg-slate-900 bg-opacity-100 backdrop-blur-sm">
+        <Link href="/" passHref>
+          <div className="flex flex-row items-center gap-x-1.5">
+            <img
+              className="mb-1"
+              src="/grinning-cat-with-smiling-eyes.png"
+              alt="home header image"
+              width={28}
+              height={28}
+            />
+            <p className="font-bold">Osin.</p>
+          </div>
+        </Link>
+        <svg
+          onClick={handleMenu}
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 8h16M4 16h16"
+          />
+        </svg>
+      </div>
     </header>
   );
 };
---
+++ components/HeaderMenu.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
+import { MouseEventHandler } from "react";
+
+const menuVariants = {
+  open: { scale: 1, opacity: 1 },
+  closed: { scale: 0, opacity: 0 },
+};
+
+const HeaderMenu = ({
+  handleMenu,
+}: {
+  handleMenu: MouseEventHandler<SVGSVGElement>;
+}) => {
+  return (
+    <div className="fixed z-20 w-full top-2 px-2 flex flex-col items-end">
+      <AnimatePresence>
+        <motion.div
+          className="w-full bg-slate-100 flex flex-row justify-between px-4 py-2 rounded-md origin-top-right"
+          initial="closed"
+          animate="open"
+          exit="closed"
+          variants={menuVariants}
+          transition={{ type: "tween", duration: 0.2 }}
+        >
+          <nav className="flex flex-col w-full my-2">
+            <Link href="/new-posts">
+              <a className="text-slate-900 hover:text-base-700 py-2">
+                New Posts
+              </a>
+            </Link>
+            <Link href="/categories">
+              <a className="text-slate-900 hover:text-base-700 py-2">
+                Categories
+              </a>
+            </Link>
+            <Link href="/about-me">
+              <a className="text-slate-900 hover:text-base-700 py-2">
+                About Me
+              </a>
+            </Link>
+          </nav>
+          <div>
+            <svg
+              onClick={handleMenu}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              stroke={"rgba(55, 55, 55, 0.9)"}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          </div>
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default HeaderMenu;
